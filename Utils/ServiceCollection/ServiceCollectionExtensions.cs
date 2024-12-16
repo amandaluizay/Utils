ec7589579c43ae1b1a736fa7b4358954aa40ab98
amanda <luiza.amandasilvah@gmail.com>
2024-12-16 13:52:20 -0300

    A

---
+++ Utils/ServiceCollection/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Utils.ServiceCollection
+{
+    internal static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, Assembly assembly, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
